782ce8d584a67fe7dd4cb9b72068c8391025afce
Lorenzo-Tr <endzz2209@gmail.com>
2023-02-16 11:19:23 +0100

    EnterParking, ParkingLane Hearder

--- include/EnterParking.hpp
+++ include/EnterParking.hpp
@@ -1,6 +1,24 @@
 #ifndef __ENTER_PARKING_H__
 #define __ENTER_PARKING_H__
 
+#include <ParkingLane.hpp>
 
+class EnterParking {
+  int id_;
+  ParkingLane parking_lane_;
+
+ public:
+  int GetId() const { return id_; }
+  ParkingLane GetParking_lane() const { return parking_lane_; }
+
+  void SetId(int id) { id_ = id; }
+  void SetParking_lane(ParkingLane parking_lane) {
+    parking_lane_ = parking_lane;
+  }
+
+  void addParkingLane();
+  void openParkingLane();
+  void closeParkingLane();
+};
 
 #endif
--- include/ParkingLane.hpp
+++ include/ParkingLane.hpp
@@ -1,6 +1,24 @@
 #ifndef __PARKING_LANE_H__
 #define __PARKING_LANE_H__
 
+class ParkingLane {
+  int id_;
 
+  bool status_;
+
+ public:
+  ParkingLane();
+  ~ParkingLane();
+
+  int GetId() const { return id_; }
+  bool GetStatus() const { return status_; }
+
+  void SetId(int id) { id_ = id; }
+  void SetStatus(bool status) { status_ = status; }
+
+  void open();
+  void close();
+  void weightVerification(int);
+};
 
 #endif
